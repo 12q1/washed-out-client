a76081f27d85e2ee3ada9cd4b10a4fc3f79a97d2
12q1 <thomasham89@gmail.com>
2018-11-06 15:23:48 +0100

    actions/redux/signup

--- App.js
+++ App.js
@@ -2,11 +2,12 @@
 
 import React, { Component } from 'react';
 import { View } from 'react-native';
-
+import { ApolloClient, Http} from 'apollo-client-preset'
 import SignUp from './screens/SignUp';
 import styles from './App.styles';
 
 export default class ShatApp extends Component {
+
   render() {
     return (
       <View style={styles.container}>
