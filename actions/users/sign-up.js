f2cd50862810e12f8f1541c0db89f0f9f6a23ad5
Amuru Swetha <42114626+amuru0S@users.noreply.github.com>
2018-11-08 10:43:53 +0100

    updated the sign-up action with the query

--- actions/users/sign-up.js
+++ actions/users/sign-up.js
@@ -1,24 +1,44 @@
-const api = new API({uri: "http://localhost:4000/graphql"});
-import request from 'superagent'
-const users = api.service('users');
+import ApolloClient, { gql } from "apollo-boost";
+
+
+export const client = new ApolloClient({
+    uri: "http://localhost:4000/graphql"
+});
 
 export const USER_SIGNUP_ERROR = 'USER_SIGNUP_ERROR';
+export const USER_SIGNED_UP = 'USER_SIGNED_UP'
 
 export default (user) => {
   return (dispatch) => {
-      request.post('localhost:4000/graphql').set('Content-Type', "application/json").send(user).catch(console.error)
-    dispatch({ type: API_LOADING });
-    users.create(user)
-      .then((result) => {
-        dispatch(signIn(user));
-      })
-      .catch((error) => {
-        dispatch({ type: API_ERROR, payload: error });
-        dispatch({
-          type: USER_SIGNUP_ERROR,
-          payload: error
-        })
-      });
-  }
+      querySignUp(user).then(res => {
 
-}
+        dispatch({
+            type: USER_SIGNED_UP
+        })
+      })
+        .catch((error) => {
+          dispatch({
+            type: USER_SIGNUP_ERROR,
+            payload: error
+          })
+        });
+    }
+      }
+    
+
+function querySignUp (user) {
+    return client.query({
+        query: gql`
+            mutation{
+                signUp(
+                  fullName:${user.fullName}
+                  email:${user.email}
+                password:${user.password}
+                  bio:${user.bio}
+                  picture:${user.picture}
+                ){
+                  id
+                }
+          `
+      })
+  }
