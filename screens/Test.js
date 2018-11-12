2a207297837c634175534fedaebca344944a1ccb
Stefanwullems <stefanwullems1998@gmail.com>
2018-11-12 14:29:20 +0100

    added more testing

--- screens/Test.js
+++ screens/Test.js
@@ -3,6 +3,8 @@ import { ScrollView, KeyboardAvoidingView } from "react-native";
 import TestAddUser from "../components/TestAddUser";
 import TestSetLocation from "../components/TestSetLocation";
 import TestUpdateServices from "../components/TestUpdateServices";
+import TestCreateServiceRequest from "../components/TestCreateServiceRequest";
+import TestUpdateServiceFees from "../components/TestUpdateServiceFees";
 
 export default class Test extends Component {
   render() {
@@ -12,6 +14,8 @@ export default class Test extends Component {
           <TestAddUser />
           <TestSetLocation />
           <TestUpdateServices />
+          <TestCreateServiceRequest />
+          <TestUpdateServiceFees />
         </KeyboardAvoidingView>
       </ScrollView>
     );
