bfbcc3cc264ceeb3f3dba3d90855d5328fbac2e1
amuru0S <amuru.codais@gmail.com>
2018-11-15 15:22:48 +0100

    user img redirected to profile page

--- screens/NavScreen.js
+++ screens/NavScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Header, Button, Body, Content, Text } from "native-base";
+import { Container, Button, Body, Content, Text } from "native-base";
 import { Actions } from 'react-native-router-flux';
 import { Font, AppLoading } from "expo";
 
@@ -28,11 +28,6 @@ export default class NavScreen extends React.Component {
       <Container style={{ backgroundColor: "#004466" }}>
         <Content>
           <Body>
-            <Button full rounded primary
-              style={{ margin: 30, backgroundColor: "#0086cb", width: "80%" }}
-              onPress={() => { Actions.myAccount() }}>
-              <Text>MyProfile</Text>
-            </Button>
             <Button full rounded primary
               style={{ margin: 30, backgroundColor: "#0086cb", width: "80%" }}
               onPress={() => { Actions.history() }}>
