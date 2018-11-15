aa2e13ad80fde4f558ede6fc355a5024b58bc638
Stefanwullems <stefanwullems1998@gmail.com>
2018-11-15 10:51:11 +0100

    comments now update immediately

--- reducers/selectedUser.js
+++ reducers/selectedUser.js
@@ -1,5 +1,6 @@
 import { FETCHED_ACCOUNT_DETAILS } from "../actions/users/fetch-account-details";
 import { CLEAR_ACCOUNT_DETAILS } from "../actions/users/clear-account-details";
+import { CREATED_COMMENT } from "../actions/comments/create-comment";
 
 export default (state = null, { type, payload }) => {
   switch (type) {
@@ -7,6 +8,12 @@ export default (state = null, { type, payload }) => {
       return JSON.parse(JSON.stringify(payload));
     case CLEAR_ACCOUNT_DETAILS:
       return null;
+    case CREATED_COMMENT:
+      const { comments, ...selectedUser } = state;
+      return {
+        comments: comments ? [...comments, payload] : [payload],
+        ...selectedUser
+      };
     default:
       return state;
   }
